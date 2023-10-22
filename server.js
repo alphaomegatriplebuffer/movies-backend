cf0f3f4bbed1bf70aaee6cf36eb3be44702a814e
alphaomegatriplebuffer <alphaomegatriplebuffer@gmail.com>
2023-10-22 10:34:32 -0700

    a

--- server.js
+++ server.js
@@ -1,68 +1,74 @@
 const express = require("express");
+const multer = require("multer");
 const cors = require("cors");
-const DataModel = require("./fileModel");
-require("./dbConn");
-require("dotenv").config();
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-
-const PORT = 3000;
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public/images"));
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.fieldname + "_" + file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-// });
-
-// app.post("/upload", upload.single("file"), async (req, res) => {
-//   const { title, genre, duration, description } = req.body;
-
-//   const data = {
-//     title: title,
-//     genre: genre,
-//     duration: duration,
-//     banner: req.file.filename,
-//     description: description,
-//   };
-//   console.log(data);
-
-//   const saveResult = await DataModel.insertMany(data);
-//   console.log(saveResult);
-//   res.json(saveResult);
-// });
-
-app.get("/records", async (req, res) => {
-  const data = await DataModel.find({});
-  console.log(data);
-  res.json(data);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://movies-backend-3d1e3.firebaseio.com",
 });
 
-app.get('/', (req,res)=>{
-  res.json('hello')
-})
+const db = admin.firestore();
+const moviesDb = db.collection("movies");
+
+const app = express();
+app.use(express.json())
+app.use(cors())
 
 
-require("mongoose")
-  .connect(process.env.MONGO_URI)
-  .then((res) => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on PORT: ${PORT}`);
-    });
-    console.log("Database Connected!");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+
+
+
+app.post("/", upload.single("file"), async (req, res) => {
+  const { title, genre, duration, description } = req.body;
+
+  const data = {
+    title: title,
+    genre: genre,
+    duration: duration,
+    banner: req.file.filename,
+    description: description,
+  };
+  
+  console.log(data);
+
+  const newDoc = moviesDb.doc();
+  const result = await newDoc.set(data);
+  res.json(result);
+  console.log(result);
+});
+
+
+// Endpoint to get data from Firestore
+app.get("/records", async (req, res) => {
+  const movies = await db.collection("movies")
+  const data = []
+
+  const snapshot = await movies.get();
+  snapshot.forEach(doc => {
+    console.log(doc.data());
+    data.push(doc.data())
   })
-  .catch((err) => {
-    console.log(err);
-  });
+
+  res.json(data)
 
 
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
